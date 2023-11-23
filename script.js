a89f62ee7345e7dd2eb54a8fc38390d7d121c3e5
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-23 17:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const info = document.querySelector("#browser-info");
+
+const browserName = navigator.appName;
+const browserVersion = navigator.appVersion;
+
+const browserMessage = document.createTextNode(`You are using ${browserName} version ${browserVersion}`);
+info.appendChild(browserMessage);
